6e3bc38b38b1d2668a1deac126903e2f389df311
YuliiaSydorenko <o972986990f@gmail.com>
2025-02-13 09:04:31 +0100

    correction2

--- src/store/slices/weatherSlice.ts
+++ src/store/slices/weatherSlice.ts
@@ -3,7 +3,6 @@ import { WeatherState as SliceWeatherState, WeatherData } from "./types"; // –û–
 import { RootState } from "../store"; 
 import axios from "axios";
 
-
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY ?? "";
 
 export const fetchWeather = createAsyncThunk(
